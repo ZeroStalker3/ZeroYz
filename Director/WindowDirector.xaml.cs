d681cb945c65824059be0a3ac0a491155c68e100
Zero <stalker_mr@list.ru>
2024-01-19 11:42:46 +0900

    Update

--- Director/WindowDirector.xaml.cs
+++ Director/WindowDirector.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using ZeroYz.ClassHelper;
 using ZeroYz.DataFilesApp;
 
@@ -21,20 +22,84 @@ namespace ZeroYz.Director
     /// </summary>
     public partial class WindowDirector : Window
     {
+        bool logicdate = false;
+        bool size = false;
+
         public WindowDirector()
         {
             InitializeComponent();
+
 
             TxtName.Text = UserControlHelp.LoginUser;
 
             OdbConnectHelper.entObj = new ZeroEntities();
 
+            GridListHistory.Columns[3].CanUserSort = false;
+
             GridListHistory.ItemsSource = OdbConnectHelper.entObj.History.ToList();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += IzmeneniaDannih;
+            timer.Start();
         }
 
-        private void IzmeneniaDannih()
+        private void IzmeneniaDannih(object sender, object e)
         {
+            var histories = OdbConnectHelper.entObj.History.ToList();
+            GridListHistory.ItemsSource = histories;
+            TotalTB.Text = histories.Count.ToString();
 
+            var dateupdate = histories.OrderByDescending(x => x.DateEvent).FirstOrDefault();
+            TxtDate.Text = dateupdate.DateEvent.ToShortDateString();
+
+        }
+
+        private void RbDec_Click(object sender, RoutedEventArgs e)
+        {
+            if(RbDec.IsChecked == true)
+            {
+                RbUp.IsChecked = false;
+                GridListHistory.ItemsSource = OdbConnectHelper.entObj.History.OrderByDescending(x => x.DateEvent).ToList();
+            }
+        }   
+
+        private void RbUp_Click(object sender, RoutedEventArgs e)
+        {
+            if(RbUp.IsChecked == true)
+            {
+                RbDec.IsChecked = false;
+                GridListHistory.ItemsSource = OdbConnectHelper.entObj.History.OrderBy(x => x.DateEvent).ToList();
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.;
+        }
+
+        private void Resize_Click(object sender, RoutedEventArgs e)
+        {
+            if (size == false)
+            { 
+                this.WindowState = WindowState.Maximized;
+                size = true;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                size = false;
+            }
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
